b9861a52b6c95316f66b82ede7bdb92ec7b9792b
labhk <labh.k2003@gmail.com>
2023-09-03 17:18:08 +0530

    Website setup completed

---
+++ app/page.js
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className='text-6xl font-extrabold text-teal-800 '>
+          MedStudySign.ai
+      </div>
+    </main>
+  )
+}
