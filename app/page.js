87561dc76f6859aa00c0d94fbdb99b6e3bdbdd15
labhk <labh.k2003@gmail.com>
2023-09-03 20:03:06 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,11 @@
-import Image from "next/image"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center p-24">
       <div className="mr-4">
-        <Image src="/signature.gif" alt="Signature" width={300} height={200} />
+        <img src="/signature.gif" alt="Signature" className="h-44 w-48"/>
       </div>
-      <div className='text-6xl font-extrabold text-teal-500 '>
+      <div className='text-6xl font-extrabold text-teal-900 '>
         MedStudySign.ai
       </div>
     </main>
